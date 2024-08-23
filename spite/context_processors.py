dd2f042a5fcef4d4a3442dd837a389988ed12240
sargent <tsargentmoore@gmail.com>
2024-08-23 08:29:36 +0000

    823

--- spite/context_processors.py
+++ spite/context_processors.py
@@ -1,7 +1,7 @@
 from blog.models import Post
 
 def load_posts(request):
-    posts = Post.objects.all().order_by('date_posted')
+    posts = Post.objects.all().order_by('-date_posted')
     return {
         'posts': posts,
         'spite' : len(posts)
