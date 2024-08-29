3d901206778dfe655744577bea92856b144d949e
sargent <tsargentmoore@gmail.com>
2024-08-29 03:40:23 +0000

    829

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 from .models import Post
 from django.contrib import messages
 from .forms import PostForm
@@ -110,3 +110,11 @@ def like_post(request, post_id):
             return JsonResponse({'like_count': post.like_count})
         except Post.DoesNotExist:
             return JsonResponse({'error': 'Post not found'}, status=404)
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Specify your template name
+    context_object_name = 'posts'  # Name of the context variable to use in the template
+    ordering = ['-date_posted']  # Order posts by date posted in descending order
