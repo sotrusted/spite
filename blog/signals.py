2c5b98ff1243208cdcc948b38c06bd7ba3068a81
sargent <tsargentmoore@gmail.com>
2024-10-07 21:05:40 +0000

    october 7: added pushover push notification signal so i get push notifications on spite posts

--- blog/signals.py
+++ blog/signals.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 import logging 
 from django.core.cache import cache
 from .models import Post
+from django.conf import settings 
+import requests
 
 logger = logging.getLogger('spite')
 
@@ -14,3 +16,16 @@ def clear_cache_on_post_save(sender, instance, **kwargs):
     #Cache posts 
     cache.clear()
 
+
+@receiver(post_save, sender=Post)
+def send_push_notification(sender, instance, created, **kwargs):
+    if created:
+        url = "https://api.pushover.net/1/messages.json"
+        data = {
+            "token": settings.PUSHOVER_API_TOKEN,
+            "user": settings.PUSHOVER_USER_KEY,
+            "message": f"A new post titled '{instance.title}' has been created.",
+            "title": "New Post Created",
+        }
+        response = requests.post(url, data=data)
+        logger.debug(response.json())
