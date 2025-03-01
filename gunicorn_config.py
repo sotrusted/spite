95eba2b992fca7e193d54659dccca0890b2e035b
Sargent <tsargentmoore@gmail.com>
2025-03-01 01:25:58 +0000

    228 post migration

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,27 @@
+import multiprocessing
 command = '/home/sargent/spite/.spite/bin/gunicorn'
 pythonpath = '/home/sargent/spite'
 bind = 'unix:/home/sargent/spite/gunicorn.sock'
-workers = 2
+workers = multiprocessing.cpu_count() * 2 
 threads = 2
 max_requests = 1000
 max_requests_jitter = 50
-worker_tmp_dir = '/dev/shm'
 worker_class = 'gthread'
-keepalive = 2  # Seconds to hold the connection open for reuse
+keepalive = 5  # Seconds to hold the connection open for reuse
+umask = 0o002  # Add this to ensure socket has correct permissions
+
+
 worker_connections = 1000
 timeout = 3600
 
+# Memory management
+max_requests = 1000
+max_requests_jitter = 100
+worker_tmp_dir = '/dev/shm'
+
+# Logging
+loglevel = 'debug'
+capture_output = True
+accesslog = '/home/sargent/spite/logs/gunicorn_access.log'
+errorlog = '/home/sargent/spite/logs/gunicorn_error.log'
+loglevel = 'info'
